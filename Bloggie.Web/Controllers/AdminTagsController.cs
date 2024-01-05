138a21b39f2f38405eed6dd931ecb2cdc12cfb7e
jakubiros <jakub.biros@microsoft.wsei.edu.pl>
2024-01-05 21:18:33 +0100

    Added options to read tags from database and show them on page

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -1,10 +1,19 @@
-﻿using Bloggie.Web.Models.ViewModels;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -15,10 +24,26 @@ namespace Bloggie.Web.Controllers
         [ActionName("Add")]
         public IActionResult Add(AddTagRequest addTagRequest)
         {
-            var name=addTagRequest.Name;
-            var displayName=addTagRequest.DisplayName;
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
 
-            return View("Add");
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {
+            var tags=bloggieDbContext.Tags.ToList();
+
+            return View(tags);
         }
     }
 }
