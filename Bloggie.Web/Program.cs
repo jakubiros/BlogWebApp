4059952d175160b3b183d41d16fa752d04c6264d
jakubiros <jakub.biros@microsoft.wsei.edu.pl>
2024-01-09 20:49:46 +0100

    Added support for image loading and hosting to the cloud

--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<BloggieDbContext>(options => options.UseSqlServer(
 
 builder.Services.AddScoped<ITagRepository, TagRepository>();
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
+builder.Services.AddScoped<IImageRepository,CloudinaryImageRepository>();
 
 var app = builder.Build();
 
