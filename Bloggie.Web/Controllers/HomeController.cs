80f5d793cb64059c443aea8dfee44face631cdea
jakubiros <jakub.biros@microsoft.wsei.edu.pl>
2024-01-11 17:27:00 +0100

    Added all tags on home page

--- Bloggie.Web/Controllers/HomeController.cs
+++ Bloggie.Web/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Bloggie.Web.Repositories;
+using Bloggie.Web.Models.ViewModels;
 
 namespace Bloggie.Web.Controllers
 {
@@ -9,18 +10,28 @@ namespace Bloggie.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBlogPostRepository blogPostRepository;
+        private readonly ITagRepository tagRepository;
 
-        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
+        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository, ITagRepository tagRepository)
         {
             _logger = logger;
             this.blogPostRepository = blogPostRepository;
+            this.tagRepository = tagRepository;
         }
 
         public async Task<IActionResult> Index()
         {
             var blogPosts = await blogPostRepository.GetAllAsync();
 
-            return View(blogPosts);
+            var tags= await tagRepository.GetAllAsync();
+
+            var model = new HomeViewModel
+            {
+                BlogPosts = blogPosts,
+                Tags = tags
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
